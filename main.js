0ddd38591dc600517593c021b015f1ccae56a51f
daniel Kabagambe <danielkabagambe@gmail.com>
2023-05-04 18:41:12 +0300

    final slider

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ hamburger.addEventListener("click", function (e) {
   hamburger.classList.toggle("cross");
 });
 // ===========this is the next image slider==========
+// get the element by its ID
+const timeElement = document.getElementById("current-time");
+
+// update the time every second
+setInterval(() => {
+  const now = new Date();
+  const timeString = now.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  timeElement.innerText = timeString;
+}, 1000);
