8581ec89575146fdd7e7ca943aebdc0a2450014e
daniel Kabagambe <danielkabagambe@gmail.com>
2023-05-12 13:18:05 +0300

    scroll

--- main.js
+++ main.js
@@ -45,3 +45,20 @@ mybutton.addEventListener("click", function () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+// this is setting for disabling the scroll behavior
+function preventHorizontalScroll(e) {
+  if (e.wheelDeltaX !== 0 || e.deltaX !== 0 || e.changedTouches) {
+    e.preventDefault();
+  }
+}
+
+document.addEventListener("touchmove", preventHorizontalScroll, {
+  passive: false,
+});
+document.addEventListener("mousewheel", preventHorizontalScroll, {
+  passive: false,
+});
+document.addEventListener("DOMMouseScroll", preventHorizontalScroll, {
+  passive: false,
+});
